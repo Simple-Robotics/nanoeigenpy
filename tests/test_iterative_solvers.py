cb16656a7b2ef796135a3684495896d14dac0b3f
ManifoldFR <wilson.jallet@polytechnique.org>
2025-03-28 15:13:14 +0100

    [tests] Rewrite test_iterative_solvers using pytest

--- tests/test_iterative_solvers.py
+++ tests/test_iterative_solvers.py
@@ -1,22 +1,23 @@
 import nanoeigenpy
 import numpy as np
+import pytest
 
 dim = 100
 seed = 6
 rng = np.random.default_rng(seed)
 
-clazzes = [
+_clazzes = [
     nanoeigenpy.solvers.ConjugateGradient,
     nanoeigenpy.solvers.IdentityConjugateGradient,
     nanoeigenpy.solvers.LeastSquaresConjugateGradient,
     nanoeigenpy.MINRES,
 ]
 
-for cls in clazzes:
-    print("================")
-    print(cls)
 
-    A = np.eye(dim)
+@pytest.mark.parametrize("cls", _clazzes)
+def test_solver(cls):
+    Q = np.random.randn(dim, dim)
+    A = Q.T @ Q / dim
     solver = cls(A)
 
     # Vector rhs
@@ -24,7 +25,6 @@ for cls in clazzes:
     x = rng.random(dim)
     b = A.dot(x)
     x_est = solver.solve(b)
-    print(x - x_est)
 
     assert nanoeigenpy.is_approx(x, x_est, 1e-6)
     assert nanoeigenpy.is_approx(b, A.dot(x_est), 1e-6)
@@ -35,30 +35,37 @@ for cls in clazzes:
     B = A.dot(X)
     X_est = solver.solve(B)
 
-    print("X_est :", X_est)
-
     assert nanoeigenpy.is_approx(X, X_est, 1e-6)
     assert nanoeigenpy.is_approx(B, A.dot(X_est), 1e-6)
+
+
+@pytest.mark.parametrize("cls", _clazzes)
+def test_solver_with_guess(cls):
+    Q = np.random.randn(dim, dim)
+    A = Q.T @ Q
+    solver = cls(A)
 
     # With guess
     # Vector rhs
 
-    solver = cls(A)
+    x = rng.random(dim)
     b = A.dot(x)
-    x_est = solver.solveWithGuess(b, x)
-
-    print("x_est :", x_est)
+    x_est = solver.solveWithGuess(b, x + 0.01)
 
     assert nanoeigenpy.is_approx(x, x_est, 1e-6)
     assert nanoeigenpy.is_approx(b, A.dot(x_est), 1e-6)
 
     # Matrix rhs
 
-    solver = cls(A)
+    X = rng.random((dim, 20))
     B = A.dot(X)
-    X_est = solver.solveWithGuess(B, X)
-
-    print("X_est :", X_est)
+    X_est = solver.solveWithGuess(B, X + 0.01)
 
     assert nanoeigenpy.is_approx(X, X_est, 1e-6)
     assert nanoeigenpy.is_approx(B, A.dot(X_est), 1e-6)
+
+
+if __name__ == "__main__":
+    import sys
+
+    sys.exit(pytest.main(sys.argv))
