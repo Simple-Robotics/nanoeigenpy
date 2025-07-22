c9a1abe22838935c23156be3a8a6f7222689c11f
Lucas Haubert <lucas.haubert@ens-paris-saclay.fr>
2025-07-22 20:00:53 +0200

    CI: Set prec 1e-9 to 1e-6 in iterative solvers

--- tests/test_iterative_solvers.py
+++ tests/test_iterative_solvers.py
@@ -34,13 +34,13 @@ def test_solver(cls):
     x_est = solver.solve(b)
 
     assert solver.info() == nanoeigenpy.ComputationInfo.Success
-    assert nanoeigenpy.is_approx(b, A.dot(x_est), 1e-9)
+    assert nanoeigenpy.is_approx(b, A.dot(x_est), 1e-6)
 
     X = rng.random((dim, 20))
     B = A.dot(X)
     X_est = solver.solve(B)
 
-    assert nanoeigenpy.is_approx(B, A.dot(X_est), 1e-9)
+    assert nanoeigenpy.is_approx(B, A.dot(X_est), 1e-6)
 
 
 @pytest.mark.parametrize("cls", _classes)
@@ -55,15 +55,15 @@ def test_solver_with_guess(cls):
     x_est = solver.solveWithGuess(b, x + 0.01)
 
     assert solver.info() == nanoeigenpy.ComputationInfo.Success
-    assert nanoeigenpy.is_approx(x, x_est, 1e-9)
-    assert nanoeigenpy.is_approx(b, A.dot(x_est), 1e-9)
+    assert nanoeigenpy.is_approx(x, x_est, 1e-6)
+    assert nanoeigenpy.is_approx(b, A.dot(x_est), 1e-6)
 
     X = rng.random((dim, 20))
     B = A.dot(X)
     X_est = solver.solveWithGuess(B, X + 0.01)
 
-    assert nanoeigenpy.is_approx(X, X_est, 1e-9)
-    assert nanoeigenpy.is_approx(B, A.dot(X_est), 1e-9)
+    assert nanoeigenpy.is_approx(X, X_est, 1e-6)
+    assert nanoeigenpy.is_approx(B, A.dot(X_est), 1e-6)
 
 
 @pytest.mark.parametrize("cls", _classes_bis)
@@ -78,13 +78,13 @@ def test_solver_bis(cls):
     x_est = solver.solve(b)
 
     assert solver.info() == nanoeigenpy.ComputationInfo.Success
-    assert nanoeigenpy.is_approx(b, A.dot(x_est), 1e-9)
+    assert nanoeigenpy.is_approx(b, A.dot(x_est), 1e-6)
 
     X = rng.random((dim, 20))
     B = A.dot(X)
     X_est = solver.solve(B)
 
-    assert nanoeigenpy.is_approx(B, A.dot(X_est), 1e-9)
+    assert nanoeigenpy.is_approx(B, A.dot(X_est), 1e-6)
 
 
 @pytest.mark.parametrize("cls", _classes_bis)
@@ -99,15 +99,15 @@ def test_solver_with_guess_bis(cls):
     x_est = solver.solveWithGuess(b, x + 0.01)
 
     assert solver.info() == nanoeigenpy.ComputationInfo.Success
-    assert nanoeigenpy.is_approx(x, x_est, 1e-9)
-    assert nanoeigenpy.is_approx(b, A.dot(x_est), 1e-9)
+    assert nanoeigenpy.is_approx(x, x_est, 1e-6)
+    assert nanoeigenpy.is_approx(b, A.dot(x_est), 1e-6)
 
     X = rng.random((dim, 20))
     B = A.dot(X)
     X_est = solver.solveWithGuess(B, X + 0.01)
 
-    assert nanoeigenpy.is_approx(X, X_est, 1e-9)
-    assert nanoeigenpy.is_approx(B, A.dot(X_est), 1e-9)
+    assert nanoeigenpy.is_approx(X, X_est, 1e-6)
+    assert nanoeigenpy.is_approx(B, A.dot(X_est), 1e-6)
 
 
 if __name__ == "__main__":
