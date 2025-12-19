06e1330913814e8a3e2625beb54ddd41e61ee888
Guilhem Saurel <guilhem.saurel@laas.fr>
2025-12-19 14:42:30 +0100

    nix: add eigen 5

--- flake.nix
+++ flake.nix
@@ -8,26 +8,65 @@
 
   outputs =
     inputs:
-    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = inputs.nixpkgs.lib.systems.flakeExposed;
-      perSystem =
-        { pkgs, self', ... }:
-        {
-          packages = {
-            default = self'.packages.nanoeigenpy;
-            nanoeigenpy = pkgs.python3Packages.nanoeigenpy.overrideAttrs (_: {
-              src = pkgs.lib.fileset.toSource {
-                root = ./.;
-                fileset = pkgs.lib.fileset.unions [
-                  ./CMakeLists.txt
-                  ./include
-                  ./package.xml
-                  ./src
-                  ./tests
-                ];
-              };
-            });
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } (
+      { self, lib, ... }:
+      {
+        systems = inputs.nixpkgs.lib.systems.flakeExposed;
+        flake.overlays = {
+          default = final: prev: {
+            pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
+              (python-final: python-prev: {
+                nanoeigenpy = python-prev.nanoeigenpy.overrideAttrs {
+                  src = lib.fileset.toSource {
+                    root = ./.;
+                    fileset = lib.fileset.unions [
+                      ./CMakeLists.txt
+                      ./include
+                      ./package.xml
+                      ./src
+                      ./tests
+                    ];
+                  };
+                };
+              })
+            ];
+          };
+          eigen_5 = final: _prev: {
+            eigen = final.eigen_5;
           };
         };
-    };
+        perSystem =
+          {
+            pkgs,
+            pkgs-eigen_5,
+            self',
+            system,
+            ...
+          }:
+          {
+            _module.args = {
+              pkgs = import inputs.nixpkgs {
+                inherit system;
+                overlays = [ self.overlays.default ];
+              };
+              pkgs-eigen_5 = import inputs.nixpkgs {
+                inherit system;
+                overlays = [
+                  self.overlays.eigen_5
+                  self.overlays.default
+                ];
+              };
+            };
+            apps.default = {
+              type = "app";
+              program = pkgs.python3.withPackages (_: [ self'.packages.default ]);
+            };
+            packages = {
+              default = self'.packages.nanoeigenpy;
+              nanoeigenpy = pkgs.python3Packages.nanoeigenpy;
+              nanoeigenpy-eigen_5 = pkgs-eigen_5.python3Packages.nanoeigenpy;
+            };
+          };
+      }
+    );
 }
